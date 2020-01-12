4cd1d7a7ab6c6ba3db1e96580a280b615ff364e6
Adriano V. dos Santos <adriano.v.santos@gmail.com>
2020-01-12 14:21:04 -0300

    Pacotes e startup script

--- server-application/start_app.sh
+++ server-application/start_app.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-export DB_HOST_DES="127.0.0.1"
-export DB_NAME_DES="db_des"
-export DB_USER_DES="postgres"
-export DB_PSWD_DES="pwd@123"
-export DB_PORT_DES="5434"
-export DB_SCHM_DES="public"
-export DB_POOL_MIN="10"
-export DB_POOL_MAX="10"
-
+pip install -r requirements.txt
 python main.py
